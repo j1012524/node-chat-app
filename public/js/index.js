bc62531ce3d4c51291f483be6356e65a832f51a8
Rakesh Choudavarapu <Rakesh.Choudavarapu@jda.com>
2017-09-02 07:01:35 +0530

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from:'abc@email.com',
-    text: 'hey this is andrew'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'rakesh@example.com',
-    text: 'Welcome to chat app',
-    createdAt: 333
-  });
-
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage);
+    io.emit('newMessage', {
+      from: newMessage.from,
+      test: newMessage.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
