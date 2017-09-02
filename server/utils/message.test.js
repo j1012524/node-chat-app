ad1088191a77f02fe411b3f6494b90ac25569508
Rakesh Choudavarapu <Rakesh.Choudavarapu@jda.com>
2017-09-02 13:34:36 +0530

    Add geolocation suppoer via geolocationAPI

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -9,6 +9,17 @@ describe('generateMessage', () => {
       from: 'rakesh',
       text: 'testing'
     });
+    expect(results.createdAt).toBeA('number');
+  });
+});
+
+describe('generate location message', () => {
+  it('should generate correct location object', () => {
+    var results = generateLocationMessage('testloc', 1, 1);
+    expect(results).toInclude({
+      from:'testloc'
+    });
+    expect(results.url).toEqual('https://www.google.com/maps?q=1,1');
     expect(results.createdAt).toBeA('number');
   });
 });
