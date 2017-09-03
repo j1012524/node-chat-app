dd09c0333cfa12ebd9ec7829513332d1639dcbbc
Rakesh Choudavarapu <Rakesh.Choudavarapu@jda.com>
2017-09-03 09:43:25 +0530

    send messages to people in that room

--- server/server.js
+++ server/server.js
@@ -38,13 +38,18 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', function (message, fn) {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+    if(user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
     fn();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if(user && isRealString(message.text)) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
